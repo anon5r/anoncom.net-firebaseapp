54dd7d3c883dfd72b1b9be2162e846b7950673cb
anon <anon@anoncom.net>
2019-11-24 01:28:41 +0900

    ロゴファイル名を変更

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded',() => {
     let btnSocials = document.getElementsByClassName('mdl-mega-footer--social-btn');
     if (btnSocials.length > 0) {
-        for (var i in btnSocials)
-            btnSocials[i].addEventListener('click',function(){
-                if (typeof(this.dataset.href)!=="undefined") {
-                    window.open(this.dataset.href,'_blank');
+        for (var i in btnSocials) {
+            btnSocials[i].addEventListener('click', function () {
+                if (typeof (this.dataset.href) !== "undefined") {
+                    window.open(this.dataset.href, '_blank');
                 }
             });
+        }
     }
 
 });
